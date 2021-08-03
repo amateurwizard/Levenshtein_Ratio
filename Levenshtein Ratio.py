6f36f5122b6b488f8efc0fee97c03e1bbb7c7ba9
tobechukwu.n.n@gmail.com <*nuk%Nai#dyC7b2XkuL@ZT#A&&y$MF>
2021-08-04 00:52:31 +0100

    Minor changes to terminal output.

--- Levenshtein Ratio.py
+++ Levenshtein Ratio.py
@@ -2,24 +2,22 @@ import Levenshtein
 
 
 def main():
-    file_name = "Big Buck Bunny (2028) [1080p BluRay X265 HEVC 10-bit AAC 7.1]"
     test_string = "BluRay"
+    file_name = "Big Buck Bunny (2028) [1080p BluRay X265 HEVC 10-bit AAC 7.1]"
+    distorted_data = [' A ', ' Of ', ' And ', ' The ', 'x265', 'I\'m ', 'St. ', 'Web-DL', 'WebRip', 'eztv.re',
+                      'Blu-ray', 'eztv.re', 'A.K.A.', '10bit', 'chery']
 
-    input_data = []
-    fruits = ["apple", "banana", "cherry"]
-    distorted_data = [' A ', ' Of ', ' And ', ' The ', 'x265', 'I\'m ', 'St. ', 'Web-DL', 'WebRip', 'eztv.re', 'Blu-ray', 'eztv.re', 'A.K.A.', '10bit', 'chery']
-
-    input_data = file_name.split()
+    input_data = file_name.split()      # Putting the file name into a list.
 
     print("Input Data", end=": ")
     print(input_data)
     print("\n")
 
     for index, item in enumerate(input_data):
-        for index2, item2 in enumerate(distorted_data):       # Replace broken metadata
+        for index2, item2 in enumerate(distorted_data):     # Nested for loop, to match all indexes between two lists.
             levenshtein_ratio = Levenshtein.ratio(input_data[index].lower(), distorted_data[index2].lower())
 
-            if levenshtein_ratio > 0.5:
+            if levenshtein_ratio > 0.5:     # Replace if the code if the match is good.
                 print("Input", end=": ")
                 print(input_data[index])
                 input_data[index] = distorted_data[index2]
@@ -28,10 +26,11 @@ def main():
                 print("Output", end=": ")
                 print(input_data[index])
                 print("\n")
+
     print("Output Data", end=": ")
     print(input_data)
 
-    print("\n******This is a test******")
+    print("\n******This is a test******")       # Practice test cases for finding Levenshtein ratio & distance.
     levenshtein_ratio = Levenshtein.ratio(distorted_data[10], test_string)
     print("Levenshtein Ratio (Raw)", end=": ")
     print(levenshtein_ratio)
