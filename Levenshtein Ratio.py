1d359c3c2a902b0aab5c32e26b99a7611ec6ded9
tobechukwu.n.n@gmail.com <*nuk%Nai#dyC7b2XkuL@ZT#A&&y$MF>
2021-08-04 00:24:29 +0100

    Minor changes to terminal output.

--- Levenshtein Ratio.py
+++ Levenshtein Ratio.py
@@ -11,25 +11,31 @@ def main():
 
     input_data = file_name.split()
 
+    print("Input Data", end=": ")
     print(input_data)
+    print("\n")
 
     for index, item in enumerate(input_data):
         for index2, item2 in enumerate(distorted_data):       # Replace broken metadata
             levenshtein_ratio = Levenshtein.ratio(input_data[index].lower(), distorted_data[index2].lower())
 
             if levenshtein_ratio > 0.5:
-                input_data[index] = distorted_data[index2]
                 print("Input", end=": ")
                 print(input_data[index])
+                input_data[index] = distorted_data[index2]
                 print("Levenshtein Ratio", end=": ")
                 print(levenshtein_ratio)
+                print("Output", end=": ")
+                print(input_data[index])
+                print("\n")
+    print("Output Data", end=": ")
     print(input_data)
 
     print("\n******This is a test******")
     levenshtein_ratio = Levenshtein.ratio(distorted_data[10], test_string)
     print("Levenshtein Ratio (Raw)", end=": ")
     print(levenshtein_ratio)
-    levenshtein_distance = Levenshtein.distance(distorted_data[10], test_string)
+    levenshtein_distance = Levenshtein.distance(distorted_data[10], test_string),
     print("Levenshtein Distance (Raw)", end=": ")
     print(levenshtein_distance)
 
